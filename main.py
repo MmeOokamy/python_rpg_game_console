d739e9772ca3806a26bc4f05c9efb16b87f529ef
mmeookamy <manue.ggt@gmail.com>
2021-07-02 21:51:44 +0200

    testing random solution loul

--- main.py
+++ main.py
@@ -14,6 +14,16 @@ class Fighter:
         atk = random.randint(self.min_strength, self.max_strength)
         adversary.pv -= atk
         print(f"{self.name} attack, {adversary.name} lose {atk} pv")
+        print()
+
+    def ripost(self, adversary):
+        print(f"{adversary.name} attack in turn")
+        reply = random.randint(adversary.min_strength, adversary.max_strength)
+        self.pv -= reply
+        print(f"{self.name} ripost, {adversary.name} lose {reply} pv")
+        print()
+        
+    def status(self, adversary):
         print(f"{self.name} : {self.pv} pv, {adversary.name} : {adversary.pv} pv")
 
     def healer(self):
@@ -21,16 +31,6 @@ class Fighter:
         self.heal -= 1
         self.pv += potion
         print(f"you are healed of {potion} pv")
-
-
-def choice(nb, fighter_a, fighter_b):
-    if nb == 1:
-        fighter_a.attack(fighter_b)
-        fighter_b.attack(fighter_a)
-
-    if nb == 2:
-        fighter_a.healer(fighter_a)
-        fighter_b.attack(fighter_a)
 
 
 # action
@@ -42,20 +42,31 @@ def fight():
 
     fighter_a = Fighter(fighter_a_name, pv, potion, 5, 10)
     fighter_b = Fighter(fighter_b_name, pv, potion, 0, 15)
+    try:
+        while not fighter_a.pv <= 0 or fighter_b.pv <= 0:
+            if 30 < fighter_a.pv:
+                fighter_a.attack(fighter_b)
+                fighter_b.ripost(fighter_a)
+                fighter_a.status(fighter_b)
+            if 0 < fighter_a.pv <= 30:
+                decision = int(input("choose fight for 1 or heal for 2 :"))
+                if decision == 1:
+                    fighter_a.attack(fighter_b)
+                    fighter_b.ripost(fighter_a)
+                    fighter_a.status(fighter_b)
+                elif decision == 2:
+                    fighter_a.healer()
+                    fighter_b.attack(fighter_a)
+                    fighter_a.status(fighter_b)
+        
+        if fighter_a.pv <= 0:
+            print(f"{fighter_b.name} Win!")
 
-    while not fighter_a.pv <= 0 or fighter_b.pv <= 0:
-        if 30 < fighter_a.pv <= 50:
-            choice(1, fighter_a, fighter_b)
-
-        if fighter_a.pv >= 30:
-            nb = input(f"{fighter_a.name} Attack(1) or  Heal(2) : ")
-            choice(nb, fighter_a, fighter_b)
-
-    if fighter_a.pv <= 0:
-        print(f"{fighter_b.name} Win!")
-
-    if fighter_b.pv <= 0:
-        print(f"{fighter_a.name} Win!")
+        if fighter_b.pv <= 0:
+            print(f"{fighter_a.name} Win!")
+        
+    except IOError as e:
+        print(e)
 
 
 fight()

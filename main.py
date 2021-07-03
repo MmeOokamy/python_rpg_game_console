743c7a5bab582e8ced3bce45959b4d4db0e0dc5a
MmeOokamy <manue.ggt@gmail.com>
2021-07-03 20:11:36 +0200

    Yeah my mini rpg game is end .... at the moment

--- main.py
+++ main.py
@@ -1,72 +1,80 @@
 import random
+from colorama import Fore, Style
 
 
 # Character object
 class Fighter:
-    def __init__(self, name, pv, heal, min_strength, max_strength):
+    def __init__(self, name, pv, heal, min_strength, max_strength, alive=True):
         self.name = name
         self.pv = pv
         self.heal = heal
         self.min_strength = min_strength
         self.max_strength = max_strength
+        self.alive = alive
 
     def attack(self, adversary):
         atk = random.randint(self.min_strength, self.max_strength)
         adversary.pv -= atk
-        print(f"{self.name} attack, {adversary.name} lose {atk} pv")
-        print()
+        print(Fore.GREEN + f"{self.name} attack, {adversary.name} lose {atk} pv")
 
     def ripost(self, adversary):
-        print(f"{adversary.name} attack in turn")
         reply = random.randint(adversary.min_strength, adversary.max_strength)
-        self.pv -= reply
-        print(f"{self.name} ripost, {adversary.name} lose {reply} pv")
-        print()
-        
+        adversary.pv -= reply
+        print(Fore.RED + f"{self.name} ripost, {adversary.name} lose {reply} pv")
+
     def status(self, adversary):
-        print(f"{self.name} : {self.pv} pv, {adversary.name} : {adversary.pv} pv")
+        print(Fore.BLUE + f"{self.name} : {self.pv} pv, {adversary.name} : {adversary.pv} pv")
 
     def healer(self):
         potion = random.randint(15, 50)
         self.heal -= 1
         self.pv += potion
-        print(f"you are healed of {potion} pv")
+        print(Fore.CYAN + f"you are healed of {potion} pv")
 
 
 # action
 def fight():
+    
     fighter_a_name = input("Name of first fighter : ")
     fighter_b_name = input("Name of seconde fighter : ")
+    
     pv = 50
     potion = 3
 
     fighter_a = Fighter(fighter_a_name, pv, potion, 5, 10)
-    fighter_b = Fighter(fighter_b_name, pv, potion, 0, 15)
-    try:
-        while not fighter_a.pv <= 0 or fighter_b.pv <= 0:
-            if 30 < fighter_a.pv:
+    fighter_b = Fighter(fighter_b_name, pv, 0, 5, 15)
+   
+    while fighter_a.alive or fighter_b.alive:
+        decision = input(Style.BRIGHT + Fore.CYAN + "Choose fight for 1 or heal for 2 : ")
+        try:
+            decision_nb = int(decision)
+        except:
+            print(Style.BRIGHT + Fore.RED + "You need to choose!!")
+        else:
+            print(Style.RESET_ALL)
+            if decision_nb == 1:
                 fighter_a.attack(fighter_b)
                 fighter_b.ripost(fighter_a)
                 fighter_a.status(fighter_b)
-            if 0 < fighter_a.pv <= 30:
-                decision = int(input("choose fight for 1 or heal for 2 :"))
-                if decision == 1:
-                    fighter_a.attack(fighter_b)
-                    fighter_b.ripost(fighter_a)
-                    fighter_a.status(fighter_b)
-                elif decision == 2:
-                    fighter_a.healer()
-                    fighter_b.attack(fighter_a)
-                    fighter_a.status(fighter_b)
-        
-        if fighter_a.pv <= 0:
+            elif decision_nb == 2:
+                fighter_a.healer()
+                fighter_b.attack(fighter_a)
+                fighter_a.status(fighter_b)
+            print(Style.RESET_ALL)
+
+        if fighter_b.pv <= 0 and fighter_a.pv <= 0:
+            fighter_a.alive = False
+            fighter_b.alive = False
+            print("Both fighters are K.O. !!")
+            break
+        elif fighter_a.pv <= 0:
+            fighter_b.alive = False
             print(f"{fighter_b.name} Win!")
-
-        if fighter_b.pv <= 0:
+            break
+        elif fighter_b.pv <= 0:
+            fighter_a.alive = False
             print(f"{fighter_a.name} Win!")
+            break
         
-    except IOError as e:
-        print(e)
-
 
 fight()
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Rpg Python Game
     It's just for programming an basic rpg game for my discord 
     and play with my bot Commandant Shepard !
+
+## :D
